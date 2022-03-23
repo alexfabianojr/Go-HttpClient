88816c820e8fcc026ff29f7b5740bb08d1ab3189
alexfabianojr <alexfabianojr@gmail.com>
2022-03-22 23:44:24 -0300

    http calls 1/2

---
+++ 1-HTTP-Calls/defaultProblemsExample.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
+
+func main() {
+
+	http.HandleFunc("/", rootHandler)
+	http.ListenAndServe(":8080", nil)
+}
